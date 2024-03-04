dc18c03c4c135dd7bc4a94147a2c1e2696876dfc
Tovar3698 <161945221+Tovar3698@users.noreply.github.com>
2024-03-03 21:13:01 -0500

    Create ejercicio3.py

---
+++ tecnico/2024/marzo/08-sesion-02-03/ejercicio3.py
@@ -0,0 +1,8 @@
+# Capturar el número
+n=float(input("Ingrese un número para saber si es positivo o negativo: "))
+
+# Comprobar si es positivo o negativo 
+if n>0:
+    print ("El numero",n,"es positivo")
+else:
+    print("El numero",n,"es negativo")
