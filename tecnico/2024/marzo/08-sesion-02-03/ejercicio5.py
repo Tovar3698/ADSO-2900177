59f3df7164238b4947f4d41229fe3178ccb2a4e4
Tovar3698 <161945221+Tovar3698@users.noreply.github.com>
2024-03-03 21:14:22 -0500

    Create ejercicio5.py

---
+++ tecnico/2024/marzo/08-sesion-02-03/ejercicio5.py
@@ -0,0 +1,8 @@
+# Capturar el número
+n=float(input("Ingrese un número para saber si está en un rango entre 50 y 100: "))
+
+# Comprobar si está o no en el rango
+if n>=50 and n<=100:
+    print("El número",n,"si está entre 50 y 100")
+else:
+    print("El número",n,"no está entre 50 y 100")
