3d6c1dac434f9f1b8ae935dcde58d9a786dbccd1
Tovar3698 <161945221+Tovar3698@users.noreply.github.com>
2024-03-03 21:13:42 -0500

    Create ejercicio4.py

---
+++ tecnico/2024/marzo/08-sesion-02-03/ejercicio4.py
@@ -0,0 +1,8 @@
+# Capturar el número
+n=float(input("Ingrese un número para saber si es mayor a 200: "))
+
+# Comprobar si es mayor o menor a 200
+if n>200:
+    print ("El número",n,"es mayor que 200")
+else:
+    print ("El número",n,"es menor que 200")
