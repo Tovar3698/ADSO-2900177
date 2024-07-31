ec62d07c4e3a67c7900f23d8dc78cef86869e68c
Javier Ricardo Tovar <161945221+Tovar3698@users.noreply.github.com>
2024-07-31 00:02:03 -0500

    Update bingo.js

--- tecnico/2024/mayo/bingo html/js/bingo.js
+++ tecnico/2024/mayo/bingo html/js/bingo.js
@@ -1,64 +1,61 @@
 /**
  * Javier Ricardo Tovar T.
- * 22/07/2024
+ * 27/07/2024
  * bingo js
  */
 
-document.addEventListener('DOMContentLoaded', function() {
-    const letras = ['B', 'I', 'N', 'G', 'O'];
-    let bingo = [];
-    let contador = 0;
+ document.addEventListener('DOMContentLoaded', function() {
+            const letras = ['B', 'I', 'N', 'G', 'O'];
+            let bingo = [];
+            let contador = 0;
 
-    console.log("\nBingo");
-
-    for (let i = 0; i < 5; i++) {
-        let fila = [];
-        for (let j = 0; j < 5; j++) {
-            contador += 1;
-            fila.push(contador * 3);
-        }
-        bingo.push(fila);
-    }
-    console.log(bingo);
-
-    let printResultado = '';
-
-
-    function generarTabla(bingo, condicional) {
-        printResultado += '<table class="table"><tr>';
-        letras.forEach(letra => {
-            printResultado += `<th class="borde color-letra">${letra}</th>`;
-        });
-        printResultado += '</tr>';
-        for (let i = 0; i < 5; i++) {
-            printResultado += '<tr>';
-            for (let j = 0; j < 5; j++) {
-                let clase = condicional(i, j) ? 'color-1' : '';
-                printResultado += `<td class="borde ${clase}">${bingo[i][j]}</td>`;
+         
+            for (let i = 0; i < 5; i++) {
+                let fila = [];
+                for (let j = 0; j < 5; j++) {
+                    contador += 1;
+                    fila.push(contador * 3);
+                }
+                bingo.push(fila);
             }
-            printResultado += '</tr>';
-        }
-        printResultado += '</table>';
-    }
+            console.log(bingo);
 
+            let printResultadoXGrande = '';
+            let printResultadoXPequenas = '';
 
-    generarTabla(bingo, () => false);
+     
+            function generarTabla(bingo, condicional, elementoId) {
+                let resultado = '';
+                resultado += '<table class="table"><tr>';
+                letras.forEach(letra => {
+                    resultado += `<th class="borde color-letra">${letra}</th>`;
+                });
+                resultado += '</tr>';
+                for (let i = 0; i < 5; i++) {
+                    resultado += '<tr>';
+                    for (let j = 0; j < 5; j++) {
+                        let clase = condicional(i, j) ? 'color-1' : '';
+                        resultado += `<td class="borde ${clase}">${bingo[i][j]}</td>`;
+                    }
+                    resultado += '</tr>';
+                }
+                resultado += '</table>';
+                document.getElementById(elementoId).innerHTML = resultado;
+            }
 
-    for (let i = 0; i < 5; i++) {
-        generarTabla(bingo, (x, y) => y == i);
-    }
+         
+            generarTabla(bingo, (i, j) => i == j || i + j == 4, 'bingo-x-grande');
 
-    generarTabla(bingo, (i, j) => i == j || i + j == 4);
-
-    generarTabla(bingo, (i, j) => {
-        if (i <= 3 && j >= 1 && j <= 4 && i % 2 === 0 && j % 2 === 1) return true;
-        if (i <= 2 && j >= 1 && j <= 2 && i % 2 === 1 && j % 2 === 0) return true;
-        if (i >= 2 && j <= 3 && i % 2 === 0 && j % 2 === 0) return true;
-        if (i >= 2 && j <= 2 && i % 2 === 1 && j % 2 === 1) return true;
-        if (i >= 2 && j >= 2 && i % 2 === 0 && j % 2 === 0) return true;
-        if (i >= 2 && j >= 2 && i % 2 === 1 && j % 2 === 1) return true;
-        return false;
-    });
-
-    document.getElementById('bingo-1').innerHTML = printResultado;
-});
+            generarTabla(bingo, (i, j) => {
+                
+                if (
+                    (i == 1 && j == 1) || (i == 1 && j == 3) || 
+                    (i == 3 && j == 1) || (i == 3 && j == 3) || 
+                    (i == 2 && j == 2) || 
+                    (i == 1 && j == 2) || (i == 2 && j == 1) || (i == 2 && j == 3) || (i == 3 && j == 2) || 
+                    (i == 3 && j == 3) || (i == 3 && j == 4) || (i == 4 && j == 3) || (i == 4 && j == 4) || 
+                    (i == 2 && j == 4) || (i == 4 && j == 2) || (i == 3 && j == 2)
+                ) return true;
+                return false;
+            }, 'bingo-x-pequenas');
+        });
